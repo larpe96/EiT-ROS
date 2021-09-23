bdcd05c9263a99c17c274f014e76413e9253d0ee
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2021-09-23 13:59:37 +0200

    Added pose_estimation package

---
+++ src/pose_estimation/src/main_pose_est.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "ros/ros.h"
+#include "pose_estimation/pose_est_srv.h"
+
+bool estimate_pose(pose_estimation::pose_est_srv::Request   &req, 
+		    pose_estimation::pose_est_srv::Response  &res)
+{
+	// Only for viewving progess in the terminal
+	ROS_INFO("pose was estimated"); 
+	return true; 
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "pose_est_server");
+	ros::NodeHandle n; 
+	
+	ros::ServiceServer service = n.advertiseService("pose_est", estimate_pose);
+	ROS_INFO("Ready to estimate relative position of the object.");
+	ros::spin(); 
+	
+	return 0; 
+}
