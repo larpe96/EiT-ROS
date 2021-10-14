d97f42cc9bcebc58ff2c09b18626b01c119ad9c3
larpe17 <larpe17@student.sdu.dk>
2021-10-14 13:14:35 +0200

    First Master ready for test

--- src/master_pkg/src/MasterNode.h
+++ src/master_pkg/src/MasterNode.h
@@ -9,24 +9,31 @@
 #include "std_msgs/String.h"
 #include "sensor_msgs/Image.h"
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseArray.h"
+
 #include "pose_estimation/pose_est_srv.h"
+
 #include "position_controller_pkg/Tcp_move.h"
+#include "position_controller_pkg/Pre_def_pose.h"
+
 #include "master_pkg/system_state_srv.h"
 #include "master_pkg/gripper_Move.h"
+#include "master_pkg/gripper_Conf.h"
  
+#define home_pos_index  0
+#define drop_off_pos_index 1
 
 enum State
 {
-   init,
-   get_pose,
-   move_to_pose,
-   grasp_obj,
-   move_with_obj,
-   drop_obj,
-   home
+  error,
+  init,
+  get_pose,
+  move_to_pose,
+  grasp_obj,
+  move_with_obj,
+  drop_obj,
+  home
 };
-
-
 
 class MasterNode
 {
@@ -39,27 +46,40 @@ public:
 
   void stateLoop(); 
 protected:
-  pose_estimation::pose_est_srv callServicePoseEstimate();
-  position_controller_pkg::Tcp_move callServiceTcpMove();
-  master_pkg::gripper_Move callServiceGripperMove();
+  int callServicePoseEstimate();
+  bool callServiceTcpMove();
+  bool callServiceGripperMove(float width,float speed);
+  bool callServiceGripperGrasp(float width,float speed);
+  bool callServiceGripperSetForce(float force);
+  bool callServicePreMove(int pose_id);
+
+  
 
   int setupServices();
+  bool setupNodes();
 
   bool sendSystemState(master_pkg::system_state_srv::Request  &req,
                                 master_pkg::system_state_srv::Response &res);
 
   ros::ServiceClient pose_estim_client;
-  ros::ServiceClient gripper_control_client;
+
+  ros::ServiceClient gripper_move_client;
+  ros::ServiceClient gripper_grasp_client;
+  ros::ServiceClient gripper_set_force_client;
+
   ros::ServiceClient tcp_control_client;
+  ros::ServiceClient tcp_pre_def_control_client;
   
   ros::ServiceServer system_state_server;
 
   
   State state = init;
   ros::NodeHandle n;
-  const char *state_name[8] = { "init", "get_pose",
+  const char *state_name[8] = { "error","init", "get_pose",
                             "move_to_pose","grasp_obj", "move_with_obj",
                             "drop_obj", "home"};
+
+  geometry_msgs::Pose obj_pose;
 };
 
 #endif // MASTER_NODE_H
