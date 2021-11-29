897ed6ac371e09fcaae2726d688315193bdfff80
ankj17 <kjeldsgaard97@gmail.com>
2021-11-29 08:24:44 +0100

    added classifier

--- src/pose_estimation/scripts/classifier_test.py
+++ src/pose_estimation/scripts/classifier_test.py
@@ -1,91 +1,100 @@
 import os
 import cv2
 import numpy as np
+import csv
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-
-def DiffNorm(img, background):
-    
-    absdiff = cv2.absdiff(img, background)
-    # diff_square = absdiff.mul(absdiff)
-    abc = []
-
-    # print(absdiff.size)
-    a, b, c = cv2.split(absdiff, [])
-
-    diff_sum = a+b+c
-    # img_ = cv2.imshow("HELLO", diff_sum)
-    # cv2.waitKey(500)
-    # diff_norm = None
-    # diff_norm= cv2.sqrt(diff_sum)
-    return diff_sum
-
-def ErodeAndDilate(img, erodeSize, dilateSize):
-    struct_element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-
-    for i in range(erodeSize):
-        cv2.erode(img, struct_element,dst=img)
-    
-    for i in range(dilateSize):
-        cv2.dilate(img,struct_element, dst=img)
-    return img
-
-Good_list= [7, 4, 1, 8, 5, 2, 6, 9 ] # obj1
-
-
-
-class Obj_class:
+class Classifier:
     def __init__(self):
-        self.path = "/home/user/workspace/src/pose_estimation/data/obj_1/"
-        #self.background = cv2.imread("/home/user/workspace/src/pose_estimation/src/backgroundEmpty.png") 
+        #self.path = "/home/user/workspace/src/pose_estimation/data/V0/data_v0.csv"
+        self.path = "./matlab_data_v1.csv"
         self.files = []
+
+        self.X_train = []
+        self.X_test = []
+        self.Y_train = []
+        self.Y_test = []
+
+        self.model = LinearDiscriminantAnalysis(solver='lsqr')
+
+        self.width = []
+        self.height = []
+        self.center_x = []
+        self.center_y = []
+        self.angle = []
+        self.id = []
+
         #self.setImgs()
 
-    def setImgs(self):
-        files = os.listdir(self.path)
-        for file in files:
-            if file.endswith(".png"):
-                self.files.append(file)
+    def loadCSV(self):
+        with open(self.path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter = ',')
 
-    def run(self):
-        humoments_tot = np.zeros([8,7])
-        i=0
-        for number in Good_list:
-            name = "rgb_obj1_"+str(number)+".png"
-            path_name = self.path+name
-            img = cv2.imread(path_name,cv2.IMREAD_GRAYSCALE)
+            for row in csv_reader:
+                self.center_x.append(float(row[1]))
+                self.center_y.append(float(row[2]))
+                self.width.append(float(row[3]))
+                self.height.append(float(row[4]))
+                self.id.append(int(row[5]))
+                self.angle.append(float(row[6]))
 
-            # new_img = DiffNorm(img, self.background)
-            bin_img = cv2.threshold(img, 150, 255,cv2.THRESH_BINARY)[1]
-            bin_img = ErodeAndDilate(bin_img,3,2)
+    def split_data(self, X, Y, test_size):
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(X, Y, test_size = test_size, random_state = 0)
 
-            moments = cv2.moments(bin_img)
-            hu_moments = cv2.HuMoments(moments)
-            
-            humoments_tot[i][0]= hu_moments[0]
-            humoments_tot[i][1]= hu_moments[1]
-            humoments_tot[i][2]= hu_moments[2]
-            humoments_tot[i][3]= hu_moments[3]
-            humoments_tot[i][4]= hu_moments[4]
-            humoments_tot[i][5]= hu_moments[5]
-            humoments_tot[i][6]= hu_moments[6]
-            i +=1
-            # print(humoments_tot)
+    def fit_data(self):
+        self.model.fit(self.X_train, self.Y_train)
 
-            # # if img == None:
-            # #     print("Could not find image in "+ path_name)
-            # # else:
-            im2, contours, hierachy = cv2.findContours(bin_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            if len(contours) >1:
-                cv2.drawContours(img, contours, 1, (0,255,0),3)
-            cv2.imshow(name,img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+    def predict_class(self, data):
+        return self.model.predict(data)
 
-        np.savetxt("test.csv", humoments_tot,delimiter=',')
+    def predict_probability(self, data):
+        return self.model.predict_proba(data)
 
+    def train_model(self):
+        x1 = np.resize(np.array(classifier.width), (-1,1))
+        x2 = np.resize(np.array(classifier.height), (-1,1))
 
+        X = np.hstack((x1, x2))
+        Y = np.array(classifier.id[:-1])
 
+        print("X shape: ", X.shape)
+        print("Y shape: ", Y.shape)
+
+        self.split_data(X, Y, 0.7)
+        self.fit_data()
+        self.save_model()
+
+    def save_model(self):
+        filename = 'model.sav'
+        pickle.dump(self.model, open(filename, 'wb'))
+
+    def load_model(self):
+        filename = 'model.sav'
+        loaded_model = pickle.load(open(filename, 'rb'))
+        self.model = loaded_model
+
+    def classify(self, data):
+        self.load_model()
+        label = self.predict_class(data)
+        prob = np.max(self.predict_probability(data))
+        return int(label), float(prob)
 
 if __name__ =="__main__":
-    classifier = Obj_class()
-    classifier.run()
+    classifier = Classifier()
+    classifier.loadCSV()
+
+    classifier.train_model()
+    prediction = classifier.predict_class(classifier.X_test)
+    probability = classifier.predict_probability(classifier.X_test)
+    score = cross_val_score(classifier.model, classifier.X_test, classifier.Y_test, cv=10)
+
+    print(score.mean())
+    print(score.std())
+
+    test_array = classifier.X_test[1,:]
+    print("test array size: ", test_array.shape)
+    test = classifier.classify(test_array.reshape((-1,2)))
+    print(test)
