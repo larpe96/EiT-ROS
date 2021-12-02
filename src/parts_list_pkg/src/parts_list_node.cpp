104081ef609c6bb9debdbd0d71e7ef3d8afd0642
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2021-12-02 13:26:29 +0100

    Added parts_list package - service tested with only individual package running

---
+++ src/parts_list_pkg/src/parts_list_node.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <string>
+#include "parts_list_pkg/partslist.h"
+#include "parts_list_pkg/missing_parts.h"
+
+PartsList bill;
+
+bool MP_service_handler(parts_list_pkg::missing_parts::Request &req, parts_list_pkg::missing_parts::Response  &res)
+{
+    res.missing_parts = bill.checkMissingParts(req.detected_parts);
+    return true; 
+}
+
+int main(int argc, char** argv)
+{
+  
+  // Setup parts list object
+  std::string partslist_file = "../EiT-ROS/src/parts_list_pkg/etc/parts_file.txt"; 
+  bill.loadPartsList(partslist_file);
+
+  //Setup ros Node/service
+  ros::init(argc, argv, "parts_list_server");
+  ros::NodeHandle n;
+  ros::ServiceServer service = n.advertiseService("missing_parts_srv", MP_service_handler);
+  ROS_INFO("Ready to find missing parts");
+  ros::spin();
+}
